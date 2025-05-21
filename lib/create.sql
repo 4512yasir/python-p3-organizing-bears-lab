a9fd577e8c6fd416b8c27336dd7e1714f32bc1b4
4512yasir <yasiryaska1@gmail.com>
2025-05-21 16:59:49 +0300

    sql creating and inserting and select

---
+++ lib/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE bears (
+   id INTEGER PRIMARY KEY,
+   name TEXT,
+   age INTEGER,
+   sex TEXT,
+   color TEXT,
+   temperament TEXT,
+   alive BOOLEAN
+   );
+
+
+-- SELECT [names of columns we are going to select] FROM [table we are selecting from];
+-- UPDATE cats SET name = "Hana" WHERE name = "Hannah";
+-- DELETE FROM cats WHERE id = 3;
